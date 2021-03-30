ddacee8087ebe728a89c5ee857459c53fb489e49
Brian Mann <mannb986@gmail.com>
2021-03-30 18:38:11 +0200

    added card deck

--- script.py
+++ script.py
@@ -1,2 +1,12 @@
-x = input('Enter your name:')
-print('Hello, ' + x)
+# Blackjack game in Python terminal 
+
+# Setting up player name
+player_name = input('Enter your name:')
+print('Hello, ' + player_name + "let's play Blackjack!")
+
+# Setting up card deck 
+jack = 10
+king = 10
+queen = 10
+ace = [1, 11]
+card_deck = [2, 3, 4, 5, 6, 7, 8, 9, 10, jack, queen, king, ace]
